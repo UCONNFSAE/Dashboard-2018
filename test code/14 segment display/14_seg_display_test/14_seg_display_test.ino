bd1fdc1237c0a391bb5c224ce0d92914afea16d6
Ryan M Hyatt <ryan.hyatt@uconn.edu>
2017-10-14 16:01:35 -0400

    Changed block to loop

--- test code/14 segment display/14_seg_display_test/14_seg_display_test.ino
+++ test code/14 segment display/14_seg_display_test/14_seg_display_test.ino
@@ -11,8 +11,25 @@ void setup()
 {
   alpha4.begin(0x70);
   alpha4.setBrightness(1);
+}
 
-  for(int i = 0; i <= s.length(); i++)
+void loop()
+{
+    for(int i = 0; i <= s.length(); i++)
+    {
+      for(int j = 0; j < 4; j++)
+      {
+        alpha4.writeDigitAscii(j,s[i+j]);
+        if(i+j >= s.length())
+        {
+          alpha4.writeDigitAscii(j, ' ');
+        }
+      }
+      
+    delay(200);
+    alpha4.writeDisplay();
+    }
+    for(int i = s.length(); i >= 0; i--)
   {
     alpha4.writeDigitAscii(3,s[i+3]);
     alpha4.writeDigitAscii(2,s[i+2]);
@@ -24,13 +41,8 @@ void setup()
     if(i+1 >= s.length()) alpha4.writeDigitAscii(1,' ');
     if(i >= s.length()) alpha4.writeDigitAscii(0,' ');
     
-    delay(400);
+    delay(200);
     alpha4.writeDisplay();
   }
-}
-
-void loop()
-{
-  
 
 }
